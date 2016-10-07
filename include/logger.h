6a80680356501d673f4a46ef4aef18475f3f6a25
pfjhyyj <pfjhyyj@qq.com>
2016-10-07 21:55:44 +0800

    Finished logger without test

--- include/logger.h
+++ include/logger.h
@@ -12,7 +12,9 @@ namespace XXYY {
 
 class Logger {
   public:
-  	Logger() = default;
+  	Logger() :time(0), processed(0), q_land(0), q_takeoff(0), ac_land(0),
+  	    ac_takeoff(0), re_land(0), re_takeoff(0), land(0), takeoff(0),
+  	    l_land(0), l_takeoff(0), idle(0), l_time(0), t_time(0), empty(true) {}
 
   	//  log all the current time unit's events into log_manager
   	void LogAll();
@@ -25,15 +27,33 @@ class Logger {
   	void Land(BasicPlane plane, uint64_t time);
   	void ReadyToTakeOff(BasicPlane plane);
   	void ReadyToLand(BasicPlane plane);
-  	void Reject(BasicPlane plane);
+  	void RejectCome(BasicPlane plane);
+  	void RejectTakeOff(BasicPlane plane);
 
   	// print log function
   	void Print(uint64_t start, uint64_t end);
+  	void PrintAll();
 
   	//  TODO:WriteToFile()
   private:
   	std::vector<std::vector<std::string> > log_manager;
   	std::vector<std::string> log_temp;
+  	uint64_t time;
+  	uint64_t processed;
+  	uint64_t q_land;
+  	uint64_t q_takeoff;
+  	uint64_t ac_land;
+  	uint64_t ac_takeoff;
+  	uint64_t re_land;
+  	uint64_t re_takeoff;
+  	uint64_t land;
+  	uint64_t takeoff;
+  	uint64_t l_land;
+  	uint64_t l_takeoff;
+  	uint64_t idle;
+  	uint64_t l_time;
+  	uint64_t t_time;
+  	bool empty;
 };
 
 } // XXYY
--- src/logger.cpp
+++ src/logger.cpp
@@ -3,8 +3,13 @@
 using XXYY::Logger;
 
 void Logger::LogAll() {
+    if (!empty) {
+        idle++;
+    }
     log_manager.push_back(log_temp);
     log_temp.clear();
+    time++;
+    empty = true;
 }
 
 void Logger::LogOne(std::string record) {
@@ -16,6 +21,10 @@ void Logger::TakeOff(BasicPlane plane, uint64_t time) {
         "Plane number " + std::to_string(plane.id) + " take off after " +
         std::to_string(time - plane.arrive_time) +
         " time units in the takeoff queue.");
+    l_takeoff--;
+    takeoff++;
+    t_time += time - plane.arrive_time;
+    empty = false;
 }
 
 void Logger::Land(BasicPlane plane, uint64_t time) {
@@ -23,18 +32,41 @@ void Logger::Land(BasicPlane plane, uint64_t time) {
         "Plane number " + std::to_string(plane.id) + " landed after " +
         std::to_string(time - plane.arrive_time) +
         " time units in the land queue.");
+    l_land--;
+    land++;
+    l_time += time - plane.arrive_time;
+    empty = false;
 }
 
 void Logger::ReadyToTakeOff(BasicPlane plane) {
     LogOne("Plane number " + std::to_string(plane.id) + " ready to take off.");
+    processed++;
+    ac_takeoff++;
+    q_takeoff++;
+    l_takeoff++;
+
 }
 
 void Logger::ReadyToLand(BasicPlane plane) {
     LogOne("Plane number " + std::to_string(plane.id) + " ready to land.");
+    processed++;
+    ac_land++;
+    q_land++;
+    l_land++;
 }
 
-void Logger::Reject(BasicPlane plane) {
+void Logger::RejectTakeOff(BasicPlane plane) {
     LogOne("Plane number " + std::to_string(plane.id) + " is rejected");
+    processed++;
+    q_takeoff++;
+    re_takeoff++;
+}
+
+void Logger::RejectCome(BasicPlane plane) {
+    LogOne("Plane number " + std::to_string(plane.id) + " is rejected");
+    processed++;
+    q_land++;
+    re_land++;
 }
 
 void Logger::Print(uint64_t start, uint64_t end) {
@@ -51,3 +83,23 @@ void Logger::Print(uint64_t start, uint64_t end) {
     }
 }
 
+void Logger::PrintAll() {
+    std::cout << "Simulation has concluded after " << time << " time units\n"
+              << "Total number of planes processed "<< processed << std::endl
+              << "Total number of planes asking to land " << q_land << std::endl
+              << "Total number of planes asking to takeoff " << q_takeoff << std::endl
+              << "Total number of planes accepted for landing " << ac_land << std::endl
+              << "Total number of planes accepted for takeoff " << ac_takeoff << std::endl
+              << "Total number of planes refused for landing " << re_land << std::endl
+              << "Total number of planes refused for takeoff " << re_takeoff << std::endl
+              << "Total number of planes that landed " << land << std::endl
+              << "Total number of planes that took off" << takeoff << std::endl
+              << "Total number of planes left in landing queue " << l_land << std::endl
+              << "Total number of planes left in takeoff queue " << l_takeoff << std::endl
+              << "Percentage of time runway idle " << idle << std::endl
+              << "Average wait time in landing queue " << l_time/(time-idle) << std::endl
+              << "Average wait time in takeoff queue " << t_time/(time-idle) << std::endl
+              << "Average observed rate of planes wanting to land " << q_land/time << std::endl
+              << "Average observed rate of planes wanting to takeoff " << q_takeoff/time << std::endl
+              << std::endl;
+}
